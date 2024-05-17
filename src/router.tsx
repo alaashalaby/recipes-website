92506d7e7c6083a875681862d75d21e91fcce287
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-18 00:17:02 +0200

    Add Routes for pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,17 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import Categories from "./pages/Categories";
+import ContactUs from "./pages/ContactUs";
 
-const router = createBrowserRouter(createRoutesFromElements(
+const router = createBrowserRouter(
+  createRoutesFromElements(
     <>
-        <Route path="/" element={<h1>Home</h1>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/recipes" element={<Recipes />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/contact-us" element={<ContactUs />} />
     </>
-));
+  )
+);
 export default router;
