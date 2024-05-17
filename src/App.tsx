9795fc5d3857faf9c2e830ed30dcd9d582e72074
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-17 09:10:14 +0200

    Integrate RouterProvider into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { RouterProvider } from "react-router-dom"
+import router from "./router"
+
 const App = () => {
   return (
     <>
-      RECIPES WEBSITE
+      <RouterProvider router={router}/>
     </>
   )
 }
