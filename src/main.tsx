d80209632fb4b280a0ff62c054092dd2313089c2
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-17 09:22:35 +0200

    Add Redux Toolkit Provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from 'react-redux'
+import store from './app/store.ts'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Provider store={store}>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+    </Provider>
+  </React.StrictMode>
+);
