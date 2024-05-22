19428d9df4074e3c46bf0979f07e8846ce9bd06a
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-22 15:22:10 +0200

    Add Route of Layout & Details

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,25 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
 import Categories from "./pages/Categories";
 import ContactUs from "./pages/ContactUs";
+import Layout from "./layout/Layout";
+import Details from "./pages/Details";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<Home />} />
-      <Route path="/recipes" element={<Recipes />} />
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/contact-us" element={<ContactUs />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/details/:id" element={<Details />}/>
+      </Route>
     </>
   )
 );
