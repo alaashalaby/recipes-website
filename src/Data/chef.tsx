39fceb3ad77940f3c88da84703e61b17c7f7cc70
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-22 15:24:14 +0200

    Add chef data

---
+++ src/Data/chef.tsx
@@ -0,0 +1,31 @@
+import chef1 from "../assets/chef-1.jpg";
+import chef2 from "../assets/chef-2.jpg";
+import chef3 from "../assets/chef-3.jfif";
+import chef4 from "../assets/chef-4.jpg";
+const chefsData: Chef[] = [
+  {
+    id: 1,
+    name: "Mike Hardstone",
+    specialty: "Server",
+    imageUrl: chef1,
+  },
+  {
+    id: 2,
+    name: "John Doe",
+    specialty: "Italian Cuisine",
+    imageUrl: chef2,
+  },
+  {
+    id: 3,
+    name: "Jane Smith",
+    specialty: "French Cuisine",
+    imageUrl: chef3,
+  },
+  {
+    id: 4,
+    name: "John Martin",
+    specialty: "Sous Chef",
+    imageUrl: chef4,
+  },
+];
+export default chefsData;
