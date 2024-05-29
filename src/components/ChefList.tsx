6305b29b56c0d6e94ea9f0607291a45f9d21fd38
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-30 00:28:41 +0200

    Edit in content od ChefList Component

--- src/components/ChefList.tsx
+++ src/components/ChefList.tsx
@@ -18,8 +18,8 @@ import chefsData from "../Data/chef";
 const ChefList = () => {
   return (
     <Box mt={9} py={2}>
-      <SubTitle subTitle={"Our Chef"} title={"Our Chef"} />
-      <SimpleGrid columns={[1,3,4]} spacing={5} mx={8} mt={6}>
+      <SubTitle subTitle={"Explore Our Chefs"} title={"Our Chefs"} />
+      <SimpleGrid columns={[1,2,3,4]} spacing={5} mx={8} mt={6}>
         {chefsData && chefsData.map(chef => (
           <Chef chef={chef} key={ chef.id} />
         ))}
@@ -32,11 +32,11 @@ export default ChefList;
 
 const Chef = ({chef}:{chef:Chef}) => {
   return (
-    <Box shadow="lg" borderRadius="lg" p={3}>
+    <Box shadow="lg" borderRadius="lg" p={3} _hover={{transform:"translateY(-10px)",cursor:"pointer"}} transition="transform 0.4s ease-in-out">
       <Image src={chef.imageUrl} alt={chef.name} borderRadius="8px 8px 0 0" h={320} w="100%" objectFit="cover"/>
       <Box textAlign="center" mt={3}>
         <Heading as="h4" fontSize="xl" mb={1}>
-          John Smith
+          {chef.name}
         </Heading>
         <Text fontSize="sm">{chef.specialty}</Text>
       </Box>
