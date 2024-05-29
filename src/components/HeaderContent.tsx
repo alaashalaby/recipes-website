8f73b7f745adf18422e25b2fba6ccb29a27db9e1
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-30 00:01:49 +0200

    Edit in Header Component

--- src/components/HeaderContent.tsx
+++ src/components/HeaderContent.tsx
@@ -1,20 +1,26 @@
-import { Button,Flex,Heading, Text } from "@chakra-ui/react";
+import { Button, Center, Heading, Text } from "@chakra-ui/react";
 
 const HeaderContent = () => {
   return (
-    <Flex
-      flexDirection="column"
-      textAlign="center"
-      gap={2}
-      position="absolute"
-      top="50%"
-      left="50%"
-      transform="translate(-50%,-50%)"
-    >
+    <Center flexDirection="column" textAlign="center" gap={2} zIndex={5}>
       <Text color="#f89223">Do you like cooking?</Text>
-      <Heading as="h1" color="#fff" fontSize={{base:"xl",md:"2xl",lg:"5xl"}}>The Thatix Recipe Community</Heading>
-      <Button bg="#f89223" _hover={{opacity:"0.7"}} w="fit-content" margin="auto" color="#fff">Explore Our Recipes</Button>
-    </Flex>
+      <Heading
+        as="h1"
+        color="#fff"
+        fontSize={{ base: "xl", md: "2xl", lg: "4xl" }}
+      >
+        Welcome to Recipe Community
+      </Heading>
+      <Button
+        bg="#f89223"
+        _hover={{ opacity: "0.7" }}
+        w="fit-content"
+        margin="8px auto"
+        color="#fff"
+      >
+        Explore Our Recipes
+      </Button>
+    </Center>
   );
 };
 
