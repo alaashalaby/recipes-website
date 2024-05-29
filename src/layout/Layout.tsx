5e11d9d15a5672de3b11649775cea3f09f9dff95
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-30 00:32:52 +0200

    Add Suspense & ScrollToTop

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Suspense } from "react";
+import { Box, Progress } from "@chakra-ui/react";
+import ScrollToTop from "../utils/ScrollToTop";
 const Layout = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
-      <Footer />
+      <ScrollToTop />
+      <Suspense
+        fallback={<Progress size="xs" isIndeterminate colorScheme="orange" />}
+      >
+        <Box as="main">
+          <Outlet />
+        </Box>
+        <Footer />
+      </Suspense>
     </>
   );
 };
