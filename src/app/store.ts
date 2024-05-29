593d711d1763f4a812e229927ffff13684b6678d
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-30 00:30:23 +0200

    Add reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import latestRecipesReducer from "../RTK/features/latest-recipes/latestRecipesSlice";
+import recipesApi from "../RTK/services/recipes";
+import savedRecipesReducer from "../RTK/features/saved-recipes/savedRecipesSlice";
 const store = configureStore({
-    reducer:{}
-})
+  reducer: {
+    latestRecipesReducer: latestRecipesReducer,
+    [recipesApi.reducerPath]: recipesApi.reducer,
+    savedRecipesReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(recipesApi.middleware),
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
