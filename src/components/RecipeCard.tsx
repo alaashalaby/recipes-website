cee0b0db70406662fbc30f73471e7100ffceb673
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-31 22:34:31 +0200

    Add custom Toast & change icon when add, delete from localStorage

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,24 +1,48 @@
-import { Box, Button, Heading, Image, useToast } from "@chakra-ui/react";
+import { Box, Button, Heading, Image } from "@chakra-ui/react";
 import { BiBookmark } from "react-icons/bi";
-import { useAppDispatch } from "../app/hooks";
-import { addRecipeToSaveList } from "../RTK/features/saved-recipes/savedRecipesSlice";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import {
+  addRecipeToSaveList,
+  removeRecipeFromSaveList,
+} from "../RTK/features/saved-recipes/savedRecipesSlice";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BsBookmarkFill } from "react-icons/bs";
+import useCustomToast from "../utils/useCustomToast";
 const RecipeCard = ({ recipe }: { recipe: Meal }) => {
   const dispatch = useAppDispatch();
-  const toast = useToast();
-  const saveRecipe = (recipe: Meal) => {
-    dispatch(addRecipeToSaveList(recipe));
-    toast({
-      title: "Recipe Saved Successfully",
-      status: "success",
-      position: "top",
-      duration: 3000,
-      isClosable: true,
-    });
+  const { showCustomToast } = useCustomToast();
+  const savedRecipes = useAppSelector(
+    (state) => state.savedRecipesReducer.savedRecipes
+  );
+  const [isRecipeSaved, setIsRecipeSaved] = useState(false);
+  useEffect(() => {
+    const exist = savedRecipes?.find(
+      (saveRecipe) => saveRecipe.idMeal === recipe.idMeal
+    );
+    setIsRecipeSaved(!!exist);
+  }, [savedRecipes, recipe.idMeal]);
+  const handleSaveRecipe = () => {
+    if (isRecipeSaved) {
+      dispatch(removeRecipeFromSaveList(recipe.idMeal));
+      showCustomToast({
+        title: "Recipe Removed Successfully",
+        status: "success",
+        position: "top-left",
+      });
+    } else {
+      dispatch(addRecipeToSaveList(recipe));
+      showCustomToast({
+        title: "Recipe Saved Successfully",
+        status: "success",
+        position: "top",
+      });
+    }
+    setIsRecipeSaved(!isRecipeSaved);
   };
   return (
     <Box borderRadius="xl" shadow="md" padding={4} bg="primary.50">
-      <Box position="relative" overflow="hidden">
+      <Box position="relative" overflow="hidden" borderRadius="lg">
         <Image
           src={recipe.strMealThumb}
           alt={recipe.strMeal}
@@ -28,7 +52,8 @@ const RecipeCard = ({ recipe }: { recipe: Meal }) => {
           _hover={{ transform: "scale(1.05)" }}
         />
         <Button
-          onClick={() => saveRecipe(recipe)}
+          onClick={handleSaveRecipe}
+          p={0}
           variant="none"
           fontSize="2xl"
           bg="#fff"
@@ -37,11 +62,17 @@ const RecipeCard = ({ recipe }: { recipe: Meal }) => {
           top={2}
           right={1}
         >
-          <BiBookmark />
+          {isRecipeSaved ? <BsBookmarkFill /> : <BiBookmark />}
         </Button>
       </Box>
       <Box textAlign="center" mt={3}>
-        <Heading as="h3" fontSize="xl" noOfLines={1} mb={2} color="secondary.800">
+        <Heading
+          as="h3"
+          fontSize="xl"
+          noOfLines={1}
+          mb={2}
+          color="secondary.800"
+        >
           {recipe.strMeal}
         </Heading>
         <Link to={`/details/${recipe.idMeal}`} key={recipe.idMeal}>
