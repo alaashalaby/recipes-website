de2c10b5fc41a19bd53279819380c01e8b3cac35
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-01 01:04:44 +0200

    Edit in srollbar

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,6 +1,20 @@
 import "@fontsource/handlee";
 const styles = {
-  global:{
+  global: {
+    "::-webkit-scrollbar": {
+      width: "13px",
+    },
+    "::-webkit-scrollbar-track": {
+      backgroundColor: "#f1f1f1",
+    },
+    "::-webkit-scrollbar-thumb": {
+      backgroundColor: "primary.500",
+      borderRadius: "0.3rem",
+    },
+    "::selection": {
+      backgroundColor: "primary.500",
+      color:"#fff"
+    },
     span: {
       fontFamily: "'Handlee', cursive",
     },
