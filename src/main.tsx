7ee59ab61ea70546ce39bf280c3035ff430a56d1
Alaa Shalaby <alaashalabi011@gmail.com>
2024-05-31 02:34:52 +0200

    Add theme to ChakraProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,12 @@ import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import store from './app/store.ts'
+import theme from './theme/index.ts'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
     </Provider>
